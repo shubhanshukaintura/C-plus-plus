f4b7f376c79f5626663b54b0f7e80c830cbec0aa
shubhanshu kaintura <78632385+shubhanshukaintura@users.noreply.github.com>
2021-09-23 10:35:50 +0530

    Pre Processor working

---
+++ practical2/nohead.cpp
@@ -0,0 +1,22 @@
+#define max 100
+using namespace std;
+int main()
+{
+	cout<<"HELLO WORLD"<<max;
+	return 0;
+}
+
+/* 
+# 1 "nohead.cpp"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "nohead.cpp"
+
+using namespace std;
+int main()
+{
+ cout<<"HELLO WORLD"<<100;
+ return 0;
+}
